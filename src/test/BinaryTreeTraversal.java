77822b46b9ed9a8799158fdf7c56fbc489701cd2
scorpion <henuhaigang@163.com>
2018-07-11 18:37:47 +0800

    二叉树遍历

--- src/test/BinaryTreeTraversal.java
+++ src/test/BinaryTreeTraversal.java
@@ -1,8 +1,8 @@
 package test;
 
-import java.util.ArrayDeque;
- 
-public class BinaryTree {
+import java.util.*;
+
+public class BinaryTreeTraversal {
     static class TreeNode{
         int value;
         TreeNode left;
@@ -15,7 +15,7 @@ public class BinaryTree {
  
     TreeNode root;
  
-    public BinaryTree(int[] array){
+    public BinaryTreeTraversal(int[] array){
         root=makeBinaryTreeByArray(array,1);
     }
  
@@ -30,6 +30,7 @@ public class BinaryTree {
             if(value!=0){
                 TreeNode t=new TreeNode(value);
                 array[index]=0;
+                System.out.println(array[index]);
                 t.left=makeBinaryTreeByArray(array,index*2);
                 t.right=makeBinaryTreeByArray(array,index*2+1);
                 return t;
@@ -49,7 +50,7 @@ public class BinaryTree {
             return;
         }       
         ArrayDeque<TreeNode> stack=new ArrayDeque<TreeNode>();
-        stack.push(root);       
+        stack.push(root);
         while(stack.isEmpty()==false){
             TreeNode node=stack.pop();
             System.out.print(node.value+"    ");
@@ -97,10 +98,73 @@ public class BinaryTree {
      *            /    /\   /
      *           22   4 28 32
      */
+
+
+    /**
+     * 比较靠谱的广度优先遍历
+     * @param root
+     * @return
+     */
+    public static ArrayList<Integer> printFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> lists=new ArrayList<Integer>();
+        if(root==null)
+            return lists;
+        Queue<TreeNode> queue=new LinkedList<TreeNode>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            TreeNode tree=queue.poll();
+            if(tree.left!=null)
+                queue.offer(tree.left);
+            if(tree.right!=null)
+                queue.offer(tree.right);
+            lists.add(tree.value);
+        }
+        return lists;
+    }
+
+    /**
+     * 深度优先遍历（非递归版本）
+     * @param root
+     * @return
+     */
+    public static ArrayList<Integer> depthPrintFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> lists=new ArrayList<Integer>();
+        if(root==null)
+            return lists;
+        Stack<TreeNode> stack=new Stack<TreeNode>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode tree=stack.pop();
+            /**先向栈找中压入右节点再压入左节点，这样出栈就是先左节点后右节点了*/
+
+            if(tree.right!=null)
+                stack.push(tree.right);
+            if(tree.left!=null)
+                stack.push(tree.left);
+            lists.add(tree.value);
+        }
+        return lists;
+    }
+
+    /**
+     * 递归深度遍历
+     * @param tn
+     */
+    private void depthTraversal(TreeNode tn)
+    {
+        if (tn!=null)
+        {
+            System.out.print(tn.value+"  ");
+            depthTraversal(tn.left);
+            depthTraversal(tn.right);
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr={0,13,65,5,97,25,0,37,22,0,4,28,0,0,32,0};
-        BinaryTree tree=new BinaryTree(arr);
+        BinaryTreeTraversal tree=new BinaryTreeTraversal(arr);
         tree.depthOrderTraversal();
+        //tree.printFromTopToBottom(root);
         tree.levelOrderTraversal();
     }
 }
