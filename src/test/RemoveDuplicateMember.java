7d2a37e341b1ca1e235bff07e03d2b0255b05a2f
scopion <henuhaigang@163.com>
2020-06-16 23:24:05 +0800

    类锁，对象锁

---
+++ src/test/RemoveDuplicateMember.java
@@ -0,0 +1,32 @@
+package test;
+
+/**
+ * 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ *
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * 注意是排序数组
+ */
+public class RemoveDuplicateMember {
+    public static void main(String[] args) {
+        int[] arr={1,1,2,1,1,1};
+        System.out.println( removeDuplicates(arr));
+    }
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+
+}
